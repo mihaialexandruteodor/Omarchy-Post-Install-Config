bddb4453990cc1bbfd4c4d24b1271c96a9025a01
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-09 00:49:56 +0200

    Update nvim-java.lua

--- scripts/debugger-nvim/lua/lsp/configs/nvim-java.lua
+++ scripts/debugger-nvim/lua/lsp/configs/nvim-java.lua
@@ -1,25 +1,69 @@
+local f = require('utils.file').is_pkg_dir
+
 return {
-  'nvim-java/nvim-java',
-  event = { "BufEnter *.java" },
-  dependencies = {
-    'nvim-java/lua-async-await',
-    'nvim-java/nvim-java-core',
-    'nvim-java/nvim-java-test',
-    'nvim-java/nvim-java-dap',
-    'MunifTanjim/nui.nvim',
-    'neovim/nvim-lspconfig',
-    'mfussenegger/nvim-dap',
-    {
-      'williamboman/mason.nvim',
-      opts = {
-        registries = {
-          'github:nvim-java/mason-registry',
-          'github:mason-org/mason-registry',
-        },
-      },
-    },
-  },
-    config = function()
-      require('java').setup()
-    end
+	'nvim-java/nvim-java',
+	enabled = true,
+	dir = f('~/Workspace/java/nvim-java'),
+	config = false,
+	dependencies = {
+		{
+			'nvim-java/lua-async',
+			dir = f('~/Workspace/java/lua-async'),
+		},
+		{
+			'nvim-java/nvim-java-core',
+			dir = f('~/Workspace/java/nvim-java-core'),
+		},
+		{
+			'nvim-java/nvim-java-test',
+			dir = f('~/Workspace/java/nvim-java-test'),
+		},
+		{
+			'nvim-java/nvim-java-dap',
+			dir = f('~/Workspace/java/nvim-java-dap'),
+		},
+		{
+			'nvim-java/nvim-java-refactor',
+			dir = f('~/Workspace/java/nvim-java-refactor'),
+		},
+		{
+			'williamboman/mason.nvim',
+			opts = {
+				registries = {
+					'github:nvim-java/mason-registry',
+					'github:mason-org/mason-registry',
+				},
+			},
+		},
+		{
+			'neovim/nvim-lspconfig',
+			opts = {
+				servers = {
+					jdtls = {
+						handlers = {
+							['$/progress'] = function() end,
+						},
+						capabilities = vim.lsp.protocol.make_client_capabilities(),
+					},
+				},
+				setup = {
+					jdtls = function()
+						require('java').setup({
+							jdk = {
+								auto_install = false,
+							},
+							notifications = {
+								dap = false,
+							},
+						})
+
+						require('lspconfig').jdtls.setup({})
+
+						-- return true will skip mason-lspconfig from setting up jdtls
+						return true
+					end,
+				},
+			},
+		},
+	},
 }
