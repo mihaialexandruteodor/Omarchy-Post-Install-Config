50a031c8b631c4c7bcd8d411d612587074b3f1bb
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-08 10:53:00 +0200

    Update master_script.sh

--- master_script.sh
+++ master_script.sh
@@ -2,5 +2,5 @@
 
 echo "Starting config for $(whoami)"
 
-# Makes all .sh files in the current folder executable
-chmod +x ./*.sh
+# Makes all .sh files in the scripts folder executable
+chmod +x ./scripts/*.sh
