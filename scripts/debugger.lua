bf7fce97b6797e0efe1ebd77891b732de888dc50
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-08 18:49:02 +0200

    Update debugger.lua

--- scripts/debugger.lua
+++ scripts/debugger.lua
@@ -1,26 +1,30 @@
--- LEADER IN OMARCHY LAZY VIM IS 'SPACE'
+-- LEADER IN OMARCHY LAZYVIM IS 'SPACE'
 
 return {
   "mfussenegger/nvim-dap",
   dependencies = {
-    "rcarriga/nvim-dap-ui"
-  }
+    "rcarriga/nvim-dap-ui",
+  },
   config = function()
     local dap = require("dap")
     local dapui = require("dapui")
 
-    dap.listeners.before.attach.dapui_config = function()
+    dapui.setup()
+
+    -- Open the UI when debugging starts
+    dap.listeners.after.event_initialized["dapui_config"] = function()
       dapui.open()
     end
-    dap.listeners.before.launch.dapui_config = function()
-      dapui.open()
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
+
+    -- Close the UI when debugging stops
+    dap.listeners.before.event_terminated["dapui_config"] = function()
       dapui.close()
     end
-    dap.listeners.before.event_exited.dapui_config = function()
+    dap.listeners.before.event_exited["dapui_config"] = function()
       dapui.close()
     end
+
+    -- Keymaps
     vim.keymap.set('n', '<Leader>b', dap.toggle_breakpoint, {})
     vim.keymap.set('n', '<Leader>c', dap.continue, {})
   end,
