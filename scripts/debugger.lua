51e5c95825301ace0de9833cbb52507f84959bf0
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-08 18:21:22 +0200

    Update debugger.lua

--- scripts/debugger.lua
+++ scripts/debugger.lua
@@ -1,1 +1,8 @@
-
+return {
+  "mfussenegger/nvim-dap",
+  config = function()
+    local dap = require("dap")
+    vim.keymap.set('n', '<Leader>b', dap.toggle_breakpoint, {})
+    vim.keymap.set('n', '<Leader>c', dap.continue, {})
+  end,
+}
