87debd7dc874b7a1042f471f41062b9695d81fe1
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-08 20:21:13 +0200

    Create Main.java

---
+++ debug-test/java-debugger/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, DAP!");
+        int a = 1;
+        int b = 2;
+        int c = a + b;  // you can set a breakpoint here
+        System.out.println("Sum: " + c);
+    }
+}
+
+
+// 
