f38dc45b98b4bdef05a151176ca7b1644adbf941
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-08 21:46:04 +0200

    Update debugger.lua

--- scripts/debugger.lua
+++ scripts/debugger.lua
@@ -3,12 +3,10 @@ return {
   dependencies = {
     "rcarriga/nvim-dap-ui",
     "nvim-neotest/nvim-nio",
-    "mfussenegger/nvim-jdtls",
   },
   config = function()
     local dap = require("dap")
     local dapui = require("dapui")
-    local jdtls = require("jdtls")
 
     -- ===== DAP UI =====
     dapui.setup()
@@ -24,52 +22,4 @@ return {
     vim.keymap.set("n", "<Leader>su", dap.step_out, {})
     vim.keymap.set("n", "<Leader>dr", dap.repl.open, {})
     vim.keymap.set("n", "<Leader>dl", dap.run_last, {})
-
-    -- ===== Helper: single-file main class =====
-    local function get_single_file_main_class()
-      local bufname = vim.api.nvim_buf_get_name(0)
-      if bufname == "" then return nil end
-
-      local filename = vim.fn.fnamemodify(bufname, ":p")
-      local src_index = filename:find("/src/")
-      local class_path = filename
-      if src_index then
-        class_path = filename:sub(src_index + 5)
-      else
-        class_path = vim.fn.fnamemodify(filename, ":.")
-      end
-      class_path = class_path:gsub("%.java$", "")
-      class_path = class_path:gsub("/", ".")
-      return class_path
-    end
-
-    -- ===== Use nvim-jdtls built-in DAP setup =====
-    jdtls.setup_dap({ hotcodereplace = "auto" })
-
-    -- ===== Java Configurations =====
-    dap.configurations.java = {
-      {
-        type = "java",
-        request = "launch",
-        name = "Launch Project / Main Class",
-        mainClass = function()
-          local ok, main_class = pcall(jdtls.get_main_class)
-          if ok and main_class and main_class ~= "" then
-            return main_class
-          else
-            return get_single_file_main_class()
-          end
-        end,
-        cwd = vim.loop.cwd(),
-        console = "integratedTerminal",
-      },
-      {
-        type = "java_attach",
-        request = "attach",
-        name = "Attach to Remote JVM",
-        hostName = "127.0.0.1",
-        port = 8000,
-      },
-    }
-  end,
 }
