74756158ccf2b9dee6a42baba48b85b559ff140b
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-09 01:17:26 +0200

    Update init.lua

--- scripts/debugger-nvim/lua/plugins/init.lua
+++ scripts/debugger-nvim/lua/plugins/init.lua
@@ -17,6 +17,10 @@ if not ok then
 	return
 end
 
+require("lsp.configs.nvim-java").setup()
+require("lspconfig").jdtls.setup {}
+
+
 local theme = require("theme")
 local plugins = {
 	---- UI
@@ -69,9 +73,9 @@ local plugins = {
 
 	---- LSP/DAP
 	require("plugins.configs.mason"),
-	"neovim/nvim-lspconfig",
+	--"neovim/nvim-lspconfig",
     require("lsp.configs.dap"),
-	require("lsp.configs.nvim-java"),
+	-- require("lsp.configs.nvim-java"),
 	-- require("lsp.configs.python"),
 
 	---- Snippets
