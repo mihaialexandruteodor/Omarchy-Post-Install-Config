7d71e80bfec15823f2958d55bb4ce50949af0ea4
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-08 21:48:44 +0200

    Update installer_debugger.sh

--- scripts/installer_debugger.sh
+++ scripts/installer_debugger.sh
@@ -8,19 +8,26 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # Source and destination paths
 CONFIG_SRC="$SCRIPT_DIR/debugger.lua"
+CONFIG_SRC_NVIM="$SCRIPT_DIR/nvim-java.lua"
 CONFIG_DEST="$HOME/.config/nvim/lua/plugins/"
 
-# Verify the file exists
+# Verify the debugger file exists
 if [ ! -f "$CONFIG_SRC" ]; then
   echo "Error: debugger.lua not found in script directory ($SCRIPT_DIR)."
+  exit 1
+fi
+
+# Verify the nmvim debug file exists
+if [ ! -f "$CONFIG_SRC_NVIM" ]; then
+  echo "Error: nvim-java.lua not found in script directory ($SCRIPT_DIR)."
   exit 1
 fi
 
 # Create destination directory if needed
 mkdir -p "$CONFIG_DEST"
 
-# Move the Lua config file
+# Move the Lua config files
 mv "$CONFIG_SRC" "$CONFIG_DEST"
+mv "$CONFIG_SRC_NVIM" "$CONFIG_DEST"
 
-echo "debugger.lua moved successfully."
-echo "Run 'nvim' and LazyVim will install nvim-dap and nvim-dap-ui automatically."
+echo "files moved successfully."
