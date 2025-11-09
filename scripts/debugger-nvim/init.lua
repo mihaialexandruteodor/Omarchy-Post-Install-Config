50e825699c5997552fe7e340e9b791c271298722
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-09 02:47:18 +0200

    Update init.lua

--- scripts/debugger-nvim/init.lua
+++ scripts/debugger-nvim/init.lua
@@ -1,33 +1,19 @@
----@module 'snacks'
-if vim.env.PROF then
-  -- example for lazy.nvim
-  -- change this to the correct path for your plugin manager
-  local snacks = vim.fn.stdpath('data') .. '/lazy/snacks.nvim'
-  vim.opt.rtp:append(snacks)
-  require('snacks.profiler').startup({
-    on_stop = {
-      highlights = true, -- highlight entries after stopping the profiler
-      pick = true, -- show a picker after stopping the profiler (uses the `on_stop` preset)
-    },
-    startup = {
-      event = 'VimEnter', -- stop profiler on this event. Defaults to `VimEnter`
-      -- event = "UIEnter",
-      -- event = "VeryLazy",
-    },
-    presets = {
-      startup = { min_time = 1, sort = true },
-    },
-  })
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
+vim.opt.rtp:prepend(lazypath)
 
-require('my/global/init')
-require('my/config/options')
-require('my/config/keymaps')
-require('my/config/commands')
-require('my/config/autocommands')
-local lazy = require('my/config/lazy')
-lazy.load({
-  spec = {
-    { import = 'my.plugins' },
-  },
-})
+
+require('options')
+require('color')
+require('lazy').setup('plugins')
+require('script')
+require('mapping')
