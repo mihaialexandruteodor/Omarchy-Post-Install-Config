9c92c305bf62a9493613323cacef24069ebf890c
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-09 02:30:17 +0200

    Add files via upload

---
+++ scripts/debugger-nvim/ftplugin/java.lua
@@ -0,0 +1,13 @@
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.softtabstop = 4
+
+-- TODO (sbadragan): fixup
+-- if vim.g.myLspDisabled ~= true then
+--   -- Start a new client & server,
+--   -- or attach to an existing client & server depending on the `config.root_dir`.
+--   local myJdtls = require('my/plugins/lsp/jdtls')
+--   P('launching JDTLS')
+--
+--   require('jdtls').start_or_attach(myJdtls.getConfig())
+-- end
---
+++ scripts/debugger-nvim/ftplugin/vimwiki.vim
@@ -0,0 +1,4 @@
+" set indentation to 3 spaces so that auto-formatting will work correctly
+setlocal shiftwidth=3
+setlocal tabstop=3
+setlocal softtabstop=3
