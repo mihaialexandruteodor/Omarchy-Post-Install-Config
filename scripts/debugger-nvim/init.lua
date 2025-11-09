0c99e00e2f3950408004a6d24f37207ab0fb2143
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-09 02:57:54 +0200

    Update init.lua

--- scripts/debugger-nvim/init.lua
+++ scripts/debugger-nvim/init.lua
@@ -1,19 +1,28 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("config.settings")
+require("config.autocmds")
+require("config.keymaps.keymaps")
+require("config.env")
 
-require('options')
-require('color')
-require('lazy').setup('plugins')
-require('script')
-require('mapping')
+local Lazy = require("util.lazy")
+
+Lazy()
+
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+    { import = "plugins.lang" },
+  },
+})
