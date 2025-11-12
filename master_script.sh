0b87597de708d2ef6dea54ce37fe2fbe6c9e5f1e
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-12 22:05:27 +0200

    Update master_script.sh

--- master_script.sh
+++ master_script.sh
@@ -20,9 +20,7 @@ rm -rf ~/.config/waybar
 ./scripts/themes.sh
 ./scripts/vmware-install.sh
 #./scripts/wallpapers.sh
-
-# add clipse to hyprland conf
-echo -e "\nexec-once = clipse -listen # run listener on startup\nwindowrulev2 = float,class:(clipse) # ensure you have a floating window class set if you want this behavior\nwindowrulev2 = size 622 652,class:(clipse) # set the size of the window as necessary\nbind = SUPER, V, exec, alacritty --class clipse -e 'clipse' # Example: bind = SUPER, V, exec, alacritty --class clipse -e 'clipse'" >> ~/.config/hypr/hyprland.conf
+./scripts/echo-into-configs.sh
 
 # omarchy cleaner
 curl -fsSL https://raw.githubusercontent.com/maxart/omarchy-cleaner/main/omarchy-cleaner.sh | bash
