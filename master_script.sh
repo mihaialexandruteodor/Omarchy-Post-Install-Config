41d5aed26985304795877e64f6167cbd0094f364
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-12 17:38:56 +0200

    Update master_script.sh

--- master_script.sh
+++ master_script.sh
@@ -19,7 +19,7 @@ mv -f ./scripts/alacritty.toml "$HOME/.config/alacritty/alacritty.toml"
 #./scripts/wallpapers.sh
 
 # add clipse to hyprland conf
-echo -e "\nexec-once = clipse -listen # run listener on startup\nwindowrulev2 = float,class:(clipse) # ensure you have a floating window class set if you want this behavior\nwindowrulev2 = size 622 652,class:(clipse) # set the size of the window as necessary\nbind = SUPER, V, exec, <terminal name> --class clipse -e 'clipse' # Example: bind = SUPER, V, exec, alacritty --class clipse -e 'clipse'" >> ~/.config/hypr/hyprland.conf
+echo -e "\nexec-once = clipse -listen # run listener on startup\nwindowrulev2 = float,class:(clipse) # ensure you have a floating window class set if you want this behavior\nwindowrulev2 = size 622 652,class:(clipse) # set the size of the window as necessary\nbind = SUPER, V, exec, alacritty --class clipse -e 'clipse' # Example: bind = SUPER, V, exec, alacritty --class clipse -e 'clipse'" >> ~/.config/hypr/hyprland.conf
 
 # omarchy cleaner
 curl -fsSL https://raw.githubusercontent.com/maxart/omarchy-cleaner/main/omarchy-cleaner.sh | bash
