a638ea10e63061a3af30d61f908cd5f1fb65f2b5
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-14 14:49:15 +0200

    Update caelestia-shell.sh

--- scripts/caelestia-shell.sh
+++ scripts/caelestia-shell.sh
@@ -20,5 +20,11 @@ sed -i '/monitor = , preferred, auto, 1/c\source = ~/.config/hypr/monitors.conf'
 # set Alacritty as terminal, Super + T
 sed -i 's/^\$terminal = foot$/\$terminal = alacritty/' ~/.config/hypr/variables.conf
 
+# add tab manager
+git clone https://github.com/mihaialexandruteodor/quickshell-overview.git ~/.config/quickshell/overview
+echo 'bind = Super, TAB, exec, qs ipc -c overview call overview toggle' >> ~/.config/hypr/hyprland.conf
+echo 'exec-once = qs -c overview' >> ~/.config/hypr/hyprland.conf
+
+
 # scheme
 caelestia scheme set -n dynamic
