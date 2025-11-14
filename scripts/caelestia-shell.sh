48891ed9bf9354cdc13443663b4e4eeb1a1776c9
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-14 14:26:03 +0200

    Update caelestia-shell.sh

--- scripts/caelestia-shell.sh
+++ scripts/caelestia-shell.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 # add it to autostart
-echo 'exec-once = bash -c "caelestia shell -d"' >> "$HOME/.config/hypr/autostart.conf"
-echo 'exec-once = bash -c "caelestia scheme -n dynamic"' >> "$HOME/.config/hypr/autostart.conf"
+#echo 'exec-once = bash -c "caelestia shell -d"' >> "$HOME/.config/hypr/autostart.conf"
+#echo 'exec-once = bash -c "caelestia scheme -n dynamic"' >> "$HOME/.config/hypr/autostart.conf"
 
 # add config
 mkdir $HOME/.config/caelestia
 cp shell.json "$HOME/.config/caelestia/shell.json"
 
-# key bindings
-cp keybinds.conf "$HOME/.config/hypr/keybinds.conf"
-
 # install the dots
 git clone https://github.com/caelestia-dots/caelestia.git ~/.local/share/caelestia && ~/.local/share/caelestia/install.fish 1
+
+# monitors conf
+cp monitors.conf "$HOME/.config/hypr/monitors.conf"
 
 # scheme
 caelestia scheme set -n dynamic
