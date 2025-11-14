d73f02658b40bf6ccbf6affe72b4698f38ac2efa
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-14 13:06:42 +0200

    Update caelestia-shell.sh

--- scripts/caelestia-shell.sh
+++ scripts/caelestia-shell.sh
@@ -4,4 +4,5 @@
 echo 'exec-once = bash -c "caelestia shell -d"' >> "$HOME/.config/hypr/autostart.conf"
 
 # add config
+mkdir $HOME/.config/caelestia
 cp shell.json "$HOME/.config/caelestia/shell.json"
