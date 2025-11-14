5320e83f87da24efcc44167c653d3c82f6f1f225
Teodor Mihai <40050793+mihaialexandruteodor@users.noreply.github.com>
2025-11-14 13:18:46 +0200

    Update caelestia-shell.sh

--- scripts/caelestia-shell.sh
+++ scripts/caelestia-shell.sh
@@ -2,6 +2,7 @@
 
 # add it to autostart
 echo 'exec-once = bash -c "caelestia shell -d"' >> "$HOME/.config/hypr/autostart.conf"
+echo 'exec-once = bash -c "caelestia scheme -n dynamic"' >> "$HOME/.config/hypr/autostart.conf"
 
 # add config
 mkdir $HOME/.config/caelestia
